59aaf164df6c4c6a19232958ec9ac8ff327c11f3
RaphaelJ-S <dk391069@ens.uqam.ca>
2020-11-11 13:20:35 -0500

    Ajout des fichiers actualises du tp1

---
+++ simple.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "Basic.h"
+#include "tcv.h"
+#include "simple.h"
+
+int init_suite(void) { return 0; }
+int clean_suite(void) { return 0; }
+
+void test_validerTH_1(void) {
+  CU_ASSERT_TRUE(validerTH_1((int)BORNE_MIN_TH));
+  CU_ASSERT_TRUE(validerTH_1((int)BORNE_MIN_TH + 1)); 
+  CU_ASSERT_TRUE(validerTH_1((int)BORNE_MAX_TH - 1));
+  CU_ASSERT_TRUE(validerTH_1((int)BORNE_MAX_TH));  
+} 
+void test_validerTH_2(void) {
+  CU_ASSERT_TRUE(validerTH_2((unsigned char)BORNE_MIN_U_CHAR));
+  CU_ASSERT_TRUE(validerTH_2((unsigned char)BORNE_MIN_U_CHAR + 1));
+  CU_ASSERT_TRUE(validerTH_2((unsigned char)BORNE_MAX_U_CHAR));
+}
+void test_validerTH_3(void) {
+  CU_ASSERT_TRUE(validerTH_3((short)BORNE_MIN_TH));
+  CU_ASSERT_TRUE(validerTH_3((short)BORNE_MIN_TH + 1));
+  CU_ASSERT_TRUE(validerTH_3((short)BORNE_MAX_TH - 1));
+  CU_ASSERT_TRUE(validerTH_3((short)BORNE_MAX_TH));
+}
+
+
+void test_validerTA_1(void) {
+  CU_ASSERT_TRUE(validerTA_1((int)BORNE_MIN_TA));
+  CU_ASSERT_TRUE(validerTA_1((int)BORNE_MIN_TA + 1));
+  CU_ASSERT_TRUE(validerTA_1((int)BORNE_MAX_TA - 1));
+  CU_ASSERT_TRUE(validerTA_1((int)BORNE_MAX_TA));
+}
+void test_validerTA_2(void) {
+  CU_ASSERT_TRUE(validerTA_2((char)BORNE_MIN_S_CHAR));
+  CU_ASSERT_TRUE(validerTA_2((char)BORNE_MIN_S_CHAR + 1));
+  CU_ASSERT_TRUE(validerTA_2((char)BORNE_MAX_S_CHAR - 1));
+  CU_ASSERT_TRUE(validerTA_2((char)BORNE_MAX_S_CHAR));
+}
+void test_validerTA_3(void) {
+  CU_ASSERT_TRUE(validerTA_3((short)BORNE_MIN_TA));
+  CU_ASSERT_TRUE(validerTA_3((short)BORNE_MIN_TA + 1));
+  CU_ASSERT_TRUE(validerTA_3((short)BORNE_MAX_TA - 1));
+  CU_ASSERT_TRUE(validerTA_3((short)BORNE_MAX_TA));;
+}
+
+
+void test_validerPulsation_1(void) {
+  CU_ASSERT_TRUE(validerPulsation_1((int)BORNE_MIN_PULSE));
+  CU_ASSERT_TRUE(validerPulsation_1((int)BORNE_MIN_PULSE + 1));
+  CU_ASSERT_TRUE(validerPulsation_1((int)BORNE_MAX_PULSE - 1));
+  CU_ASSERT_TRUE(validerPulsation_1((int)BORNE_MAX_PULSE));
+}
+void test_validerPulsation_2(void) {
+  CU_ASSERT_TRUE(validerPulsation_2((char)BORNE_MIN_PULSE));
+  CU_ASSERT_TRUE(validerPulsation_2((char)BORNE_MIN_PULSE + 1));
+  CU_ASSERT_TRUE(validerPulsation_2((char)BORNE_MAX_S_CHAR - 1));
+  CU_ASSERT_TRUE(validerPulsation_2((char)BORNE_MAX_S_CHAR));
+}
+void test_validerPulsation_3(void) {
+  CU_ASSERT_TRUE(validerPulsation_3((short)BORNE_MIN_PULSE));
+  CU_ASSERT_TRUE(validerPulsation_3((short)BORNE_MIN_PULSE + 1));
+  CU_ASSERT_TRUE(validerPulsation_3((short)BORNE_MAX_PULSE - 1));
+  CU_ASSERT_TRUE(validerPulsation_3((short)BORNE_MAX_PULSE));
+}
+
+
+void test_validerSignal_1(void) {
+  CU_ASSERT_TRUE(validerSignal_1((int)BORNE_MIN_SIGNAL));
+  CU_ASSERT_TRUE(validerSignal_1((int)BORNE_MIN_SIGNAL + 1));
+  CU_ASSERT_TRUE(validerSignal_1((int)BORNE_MAX_SIGNAL - 1));
+  CU_ASSERT_TRUE(validerSignal_1((int)BORNE_MAX_SIGNAL));
+}
+void test_validerSignal_2(void) {
+  CU_ASSERT_TRUE(validerSignal_2((char)BORNE_MIN_SIGNAL));
+  CU_ASSERT_TRUE(validerSignal_2((char)BORNE_MIN_SIGNAL + 1));
+  CU_ASSERT_TRUE(validerSignal_2((char)BORNE_MAX_SIGNAL - 1));
+  CU_ASSERT_TRUE(validerSignal_2((char)BORNE_MAX_SIGNAL));
+}
+void test_validerSignal_3(void) {
+  CU_ASSERT_TRUE(validerSignal_3((short)BORNE_MIN_SIGNAL));
+  CU_ASSERT_TRUE(validerSignal_3((short)BORNE_MIN_SIGNAL + 1));
+  CU_ASSERT_TRUE(validerSignal_3((short)BORNE_MAX_SIGNAL - 1));
+  CU_ASSERT_TRUE(validerSignal_3((short)BORNE_MAX_SIGNAL));
+}
---
+++ tp1a.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "Basic.h"
+#include "tcv.h"
+#include "simple.h"
+
+int main ( void )
+{
+   CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if ( CUE_SUCCESS != CU_initialize_registry() )
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite( "tp1_test_suite", init_suite, clean_suite );
+   if ( NULL == pSuite ) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+   /* add the tests to the suite */
+   if ( (NULL == CU_add_test(pSuite, "validerTH_1", test_validerTH_1)) ||
+        (NULL == CU_add_test(pSuite, "validerTH_2", test_validerTH_2)) ||	
+	(NULL == CU_add_test(pSuite, "validerTH_3", test_validerTH_3)) ||
+        (NULL == CU_add_test(pSuite, "validerTA_1", test_validerTA_1)) ||
+	(NULL == CU_add_test(pSuite, "validerTA_2", test_validerTA_2)) ||
+	(NULL == CU_add_test(pSuite, "validerTA_3", test_validerTA_3)) ||
+        (NULL == CU_add_test(pSuite, "validerPulsation_1", test_validerPulsation_1)) ||
+	(NULL == CU_add_test(pSuite, "validerPulsation_2", test_validerPulsation_2)) ||
+	(NULL == CU_add_test(pSuite, "validerPulsation_3", test_validerPulsation_3)) ||
+	(NULL == CU_add_test(pSuite, "validerSignal_1", test_validerSignal_1)) || 
+	(NULL == CU_add_test(pSuite, "validerSignal_2", test_validerSignal_2)) ||
+	(NULL == CU_add_test(pSuite, "validerSignal_3", test_validerSignal_3)) 
+      )
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+   // Run all tests using the basic interface
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   printf("\n");
+   CU_basic_show_failures(CU_get_failure_list());
+   printf("\n\n");
+/* 
+   // LES modes interactifs
+   // Run all tests using the automated interface
+   CU_automated_run_tests();
+   CU_list_tests_to_file();
+
+   // Run all tests using the console interface
+   CU_console_run_tests();
+*/
+   /* Clean up registry and return */
+}
