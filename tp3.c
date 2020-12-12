9ff42b4ba3c98440f478a82c6a24e6886ed1bd4e
RaphaelJ-S <dk391069@ens.uqam.ca>
2020-12-12 16:42:39 -0500

    tab et espace

--- tp3.c
+++ tp3.c
@@ -4,25 +4,25 @@
 int main(int argc, char** argv) {
   char str[200];
   char* ptr;
-	unsigned char flags[4]={0,0,0,0};
+  unsigned char flags[4]={0,0,0,0};
   identifiant_t identification = {9999, 2};
   Compteur_t compte = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
   idPN_t idPN = {0, malloc(sizeof(size_t)), 0};
-	version_t vers;   
+  version_t vers;   
   getVersion(&vers);
-	donnees_t data = {&vers,flags , &compte, &identification, &idPN, 0};  
-	int option = cmd(argc, argv, &data);
-	
-	if(option == 0) {
-		printf("version #: %d.%d.%d",vers.major, vers.minor, vers.build);
-  	while(fgets(str,200, stdin) != NULL) {
-				ptr = strtok(str, "\n");
-   		 	data.cmpt_s->nbrTrxTot += 1;
-				lecture(ptr, &data);
-  	}
-		affichageOptions(&data);
-	}else printf("\n\nUne option n'est pas valide. Les options sont : -d -t -i -s\n\n");
+  donnees_t data = {&vers,flags , &compte, &identification, &idPN, 0};  
+  int option = cmd(argc, argv, &data);
   
-	free(idPN.tab);
+  if(option == 0) {
+    printf("version #: %d.%d.%d",vers.major, vers.minor, vers.build);
+    while(fgets(str,200, stdin) != NULL) {
+        ptr = strtok(str, "\n");
+        data.cmpt_s->nbrTrxTot += 1;
+        lecture(ptr, &data);
+    }
+    affichageOptions(&data);
+  }else printf("\n\nUne option n'est pas valide. Les options sont : -d -t -i -s\n\n");
+  
+  free(idPN.tab);
   return 0;
 }
