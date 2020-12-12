973fe1e4a32a3c988e7019297ed1ac5d44280905
RaphaelJ-S <dk391069@ens.uqam.ca>
2020-12-12 16:43:51 -0500

    tab en espace

--- malib.c
+++ malib.c
@@ -1,118 +1,118 @@
 #include "malib.h"
 
 void lecture( char* ligne, donnees_t* data ) {
-	if(ligne != NULL) {
-		char* delim =" ";
-		bool valide = true; 
-		int i = 0;
-		
-		if(*ligne != '\0') {
-			char* partie = strtok(ligne,delim);
-    	
-			while(partie && valide) {
-      	if(i == 0) valide = confirmeTimestamp(partie, data); 
-				else if (i == 1) valide = confirmeEvenement(partie, data);
-				else if (i == 2) valide = confirmeDeux(partie, data);
-				else if (i == 3) valide = confirmeTrois(partie, data);
-				partie = strtok(NULL,delim);
-      	++i;
-    	}
-		}
-		if(validerNbrParam(data, i) && data->options[0] == 0 && valide) {
-			affichageLigne(data);
-		}
+  if(ligne != NULL) {
+    char* delim =" ";
+    bool valide = true; 
+    int i = 0;
+    
+    if(*ligne != '\0') {
+      char* partie = strtok(ligne,delim);
+      
+      while(partie && valide) {
+        if(i == 0) valide = confirmeTimestamp(partie, data); 
+        else if (i == 1) valide = confirmeEvenement(partie, data);
+        else if (i == 2) valide = confirmeDeux(partie, data);
+        else if (i == 3) valide = confirmeTrois(partie, data);
+        partie = strtok(NULL,delim);
+        ++i;
+      }
+    }
+    if(validerNbrParam(data, i) && data->options[0] == 0 && valide) {
+      affichageLigne(data);
+    }
   }     
 }
 
 bool confirmeDeux(char* partie, donnees_t* data) {
-	char event = data->event;
-	int err = strcmp(partie, "ERREUR");
-	
-	if(event == 0) {
-		data->identif_s->id = atol(partie);
-	}else if (event == 1 || event == 2 || event == 3 || event == 4){
-		if(err == 0){
-			if(event == 1) data->cmpt_s->manifErrTH += 1;
-			if(event == 2) data->cmpt_s->manifErrTA += 1;
-			if(event == 3) data->cmpt_s->manifErrPulse += 1;
-			return false;
-		}else {
-			data->degSig = atof(partie);
-			data->pwrSig = atoi(partie);
-			return validerDegSig(data);
-		}
+  char event = data->event;
+  int err = strcmp(partie, "ERREUR");
+  
+  if(event == 0) {
+    data->identif_s->id = atol(partie);
+  }else if (event == 1 || event == 2 || event == 3 || event == 4){
+    if(err == 0){
+      if(event == 1) data->cmpt_s->manifErrTH += 1;
+      if(event == 2) data->cmpt_s->manifErrTA += 1;
+      if(event == 3) data->cmpt_s->manifErrPulse += 1;
+      return false;
+    }else {
+      data->degSig = atof(partie);
+      data->pwrSig = atoi(partie);
+      return validerDegSig(data);
+    }
   } else if( event == 5 ) { 
-		int (*comp)(const void*, const void*) = comparer;
-		size_t id = atol(partie);
-		
-		if(data->id_s->size > 1) qsort( data->id_s->tab, data->id_s->size, sizeof(size_t), (*comp) ); 		
-		if( !(bsearch( (void*)&id, data->id_s->tab, data->id_s->size, sizeof(size_t), (*comp))) ) ajouterIdPN(data->id_s, id);
+    int (*comp)(const void*, const void*) = comparer;
+    size_t id = atol(partie);
+    
+    if(data->id_s->size > 1) qsort( data->id_s->tab, data->id_s->size, sizeof(size_t), (*comp) );     
+    if( !(bsearch( (void*)&id, data->id_s->tab, data->id_s->size, sizeof(size_t), (*comp))) ) ajouterIdPN(data->id_s, id);
 
-		qsort( data->id_s->tab, data->id_s->size, sizeof(size_t), (*comp) );
-	}	
-	return true;
+    qsort( data->id_s->tab, data->id_s->size, sizeof(size_t), (*comp) );
+  } 
+  return true;
 }
 
 bool confirmeTrois(char* partie, donnees_t* data) {
-	char event = data->event;
-	
-	if(event == 0) {
-		int pwr = atoi(partie);
-		
-		if(pwr == 2||pwr == 3||pwr == 4) {
-			data->identif_s->puissance = (unsigned char)pwr;	
-		}else {
-			data->identif_s->puissance = (unsigned char)2;
-			return false;
-		}
-	}else if(event == 4) {
-		ajouterIdPN(data->id_s, atol(partie));
-		data->id_s->id = atol(partie);
-	}
-	return true;
+  char event = data->event;
+  
+  if(event == 0) {
+    int pwr = atoi(partie);
+    
+    if(pwr == 2||pwr == 3||pwr == 4) {
+      data->identif_s->puissance = (unsigned char)pwr;  
+    }else {
+      data->identif_s->puissance = (unsigned char)2;
+      return false;
+    }
+  }else if(event == 4) {
+    ajouterIdPN(data->id_s, atol(partie));
+    data->id_s->id = atol(partie);
+  }
+  return true;
 }
 
 bool confirmeEvenement(char* partie, donnees_t* data) {
-	int evenement = validerEvenement(partie);
+  int evenement = validerEvenement(partie);
   
-	if(evenement == -1) { 
-		data->cmpt_s->trxInval += 1;
-		return false;	
-	}else if ( evenement == 1) data->cmpt_s->nbrTH += 1;
-	else if ( evenement == 2) data->cmpt_s->nbrTA += 1;
-	else if ( evenement == 3) data->cmpt_s->nbrPulse += 1;
-	else if ( evenement == 4) data->cmpt_s->nbrSignal += 1;
-	else if ( evenement == 5) data->cmpt_s->nbrIdPn += 1;
-	data->event = evenement;
-	return true;
+  if(evenement == -1) { 
+    data->cmpt_s->trxInval += 1;
+    return false; 
+  }else if ( evenement == 1) data->cmpt_s->nbrTH += 1;
+  else if ( evenement == 2) data->cmpt_s->nbrTA += 1;
+  else if ( evenement == 3) data->cmpt_s->nbrPulse += 1;
+  else if ( evenement == 4) data->cmpt_s->nbrSignal += 1;
+  else if ( evenement == 5) data->cmpt_s->nbrIdPn += 1;
+  data->event = evenement;
+  return true;
 }
 
 bool validerDegSig(donnees_t* data) {
   unsigned int build = data->version->build;
-	char event = data->event;
-	int foncAPrendre = build >= 1005 && build <= 1008 ? 1 : build <= 1003 ? 2 : 0; 
-	float degSig = data->degSig;
-	bool valide = false;
-	
-	if(event == 1) {
-		valide = foncAPrendre == 1 || foncAPrendre == 2 ? validerTH_1((int)(degSig * 10)) : false;
-		if(valide) data->cmpt_s->sumTH += degSig; 
-		else data->cmpt_s->valInvTH += 1; 
-	
-	}else if (event == 2) {
-		valide = foncAPrendre == 1 ? validerTA_1((int)(degSig*10)) : foncAPrendre == 2 ? validerTA_3((short)(degSig * 10)) : false;
-		if(valide) data->cmpt_s->sumTA += degSig;
-		else data->cmpt_s->valInvTA += 1;
-	
-	}else if (event == 3) {
-		valide = foncAPrendre == 1 ? validerPulsation_1((int)degSig) : foncAPrendre == 2 ? validerPulsation_3((short)degSig) : false;
-		if(valide)	data->cmpt_s->sumPulse += degSig;
-		else 	data->cmpt_s->valInvPulse += 1;
-	
-	}else if (event == 4) {
-		valide = foncAPrendre == 1 ? validerSignal_3((short)degSig) : foncAPrendre == 2 ? validerSignal_2((char)degSig) : false;
-	}
-	return valide;
+  char event = data->event;
+  int foncAPrendre = build >= 1005 && build <= 1008 ? 1 : build <= 1003 ? 2 : 0; 
+  float degSig = data->degSig;
+  bool valide = false;
+  
+  if(event == 1) {
+    valide = foncAPrendre == 1 || foncAPrendre == 2 ? validerTH_1((int)(degSig * 10)) : false;
+    if(valide) data->cmpt_s->sumTH += degSig; 
+    else data->cmpt_s->valInvTH += 1; 
+  
+  }else if (event == 2) {
+    valide = foncAPrendre == 1 ? validerTA_1((int)(degSig*10)) : foncAPrendre == 2 ? validerTA_3((short)(degSig * 10)) : false;
+    if(valide) data->cmpt_s->sumTA += degSig;
+    else data->cmpt_s->valInvTA += 1;
+  
+  }else if (event == 3) {
+    valide = foncAPrendre == 1 ? validerPulsation_1((int)degSig) : foncAPrendre == 2 ? validerPulsation_3((short)degSig) : false;
+    if(valide)  data->cmpt_s->sumPulse += degSig;
+    else  data->cmpt_s->valInvPulse += 1;
+  
+  }else if (event == 4) {
+    valide = foncAPrendre == 1 ? validerSignal_3((short)degSig) : foncAPrendre == 2 ? validerSignal_2((char)degSig) : false;
+  }
+  return valide;
 }
 
 int validerEvenement(char* partie) {
@@ -129,21 +129,21 @@ int validerEvenement(char* partie) {
 }
 
 bool confirmeTimestamp(char* partie, donnees_t* data) {
-	size_t timestamp = atol(partie);
-	
-	if(data->timestamp > timestamp) {
-		data->cmpt_s->nbrNonSeq += 1;
-		return false;
-	}else {
-		data->timestamp = timestamp;
-		return true;
-	}
+  size_t timestamp = atol(partie);
+  
+  if(data->timestamp > timestamp) {
+    data->cmpt_s->nbrNonSeq += 1;
+    return false;
+  }else {
+    data->timestamp = timestamp;
+    return true;
+  }
 }
 bool validerNbrParam(donnees_t* data, int size) {
   int evenement = data->event;
-	
-	if(size < 3)return false;
-	return ((evenement == 0 ||evenement == 4) ? (size != 4 ? false : true) 
+  
+  if(size < 3)return false;
+  return ((evenement == 0 ||evenement == 4) ? (size != 4 ? false : true) 
          : (evenement == 1 ||evenement == 2 ||evenement ==3) ? (size != 3 ? false : true)
          : (evenement == 5) ? (size < 4 ? false : true) 
          : false);
--- outil3.c
+++ outil3.c
@@ -4,64 +4,64 @@
 #define _M_ -69
 
 int cmd(int argc, char** argv, donnees_t* data) {
-	
-	for( int i = 1; i < argc; ++i) {
-		if( strcmp( argv[i], "-t") == 0 ) data->options[0] = (unsigned char)1;
-		else if( strcmp( argv[i], "-i" ) == 0 ) data->options[1] = (unsigned char)1;
-		else if( strcmp( argv[i], "-d" ) == 0 ) data->options[2] = (unsigned char)1;
-		else if( strcmp( argv[i], "-s" ) == 0 ) data->options[3] = (unsigned char)1;
-		else return -1;
-	}
-	return 0;
+  
+  for( int i = 1; i < argc; ++i) {
+    if( strcmp( argv[i], "-t") == 0 ) data->options[0] = (unsigned char)1;
+    else if( strcmp( argv[i], "-i" ) == 0 ) data->options[1] = (unsigned char)1;
+    else if( strcmp( argv[i], "-d" ) == 0 ) data->options[2] = (unsigned char)1;
+    else if( strcmp( argv[i], "-s" ) == 0 ) data->options[3] = (unsigned char)1;
+    else return -1;
+  }
+  return 0;
 }
 
 float distance(int _signal, int _power) {
-	return round( pow( _BASE_, ( _M_ - _signal ) / ( _BASE_ * _power )) * 10) / 10;
+  return round( pow( _BASE_, ( _M_ - _signal ) / ( _BASE_ * _power )) * 10) / 10;
 }
 
 int comparer(const void* arg1, const void* arg2) {
-	return ( *(size_t*)arg1 - *(size_t*)arg2 );
+  return ( *(size_t*)arg1 - *(size_t*)arg2 );
 }
 
 void affichageOptions(donnees_t* data) {
-	if(data->options[0] == 0) {
-		affichageCompt(data);
-	}
-	if (data->options[1] == 1){
-		printf("\ninformation invalide\n  trx non reconnue : %ld\n  trx avec ts non sequentiel : %ld", data->cmpt_s->trxInval, data->cmpt_s->nbrNonSeq);
-	}
-	if (data->options[2] == 1) {
-		printf("\ninformation detaillee\n  trx 01 : %ld\n  trx 02 : %ld\n  trx 03 : %ld\n  trx 04 : %ld\n  trx 05 : %ld\n  le dernier ts lu : %ld", data->cmpt_s->nbrTH, data->cmpt_s->nbrTA, data->cmpt_s->nbrPulse, data->cmpt_s->nbrSignal, data->cmpt_s->nbrIdPn, data->timestamp);
-	}
-	if (data->options[3] == 1) {
-		printf("\ninformation sommaire\n  nbr trx valides : %ld\n  nbr trx (total) : %ld", data->cmpt_s->nbrTrxTot - data->cmpt_s->trxInval - data->cmpt_s->nbrNonSeq, data->cmpt_s->nbrTrxTot);
-	}
+  if(data->options[0] == 0) {
+    affichageCompt(data);
+  }
+  if (data->options[1] == 1){
+    printf("\ninformation invalide\n  trx non reconnue : %ld\n  trx avec ts non sequentiel : %ld", data->cmpt_s->trxInval, data->cmpt_s->nbrNonSeq);
+  }
+  if (data->options[2] == 1) {
+    printf("\ninformation detaillee\n  trx 01 : %ld\n  trx 02 : %ld\n  trx 03 : %ld\n  trx 04 : %ld\n  trx 05 : %ld\n  le dernier ts lu : %ld", data->cmpt_s->nbrTH, data->cmpt_s->nbrTA, data->cmpt_s->nbrPulse, data->cmpt_s->nbrSignal, data->cmpt_s->nbrIdPn, data->timestamp);
+  }
+  if (data->options[3] == 1) {
+    printf("\ninformation sommaire\n  nbr trx valides : %ld\n  nbr trx (total) : %ld", data->cmpt_s->nbrTrxTot - data->cmpt_s->trxInval - data->cmpt_s->nbrNonSeq, data->cmpt_s->nbrTrxTot);
+  }
 }
 
 void affichageLigne(donnees_t* data) {
-	float (*dist)(int, int) = distance;
+  float (*dist)(int, int) = distance;
 
-	if(data->event == 0) printf("\n10 %ld %ld %d", data->timestamp, data->identif_s->id, data->identif_s->puissance);
-	if(data->event == 4) printf("\n14 %ld %ld %.1f", data->timestamp, data->id_s->id, (*dist)(data->pwrSig, data->identif_s->puissance));
-	if(data->event == 5){
-		printf("\n15 %ld %ld ", data->timestamp, data->identif_s->id);
-		for(int i = 0; i < data->id_s->size; ++i) {
-			printf("%ld ", data->id_s->tab[i]);
-		}
-	}
+  if(data->event == 0) printf("\n10 %ld %ld %d", data->timestamp, data->identif_s->id, data->identif_s->puissance);
+  if(data->event == 4) printf("\n14 %ld %ld %.1f", data->timestamp, data->id_s->id, (*dist)(data->pwrSig, data->identif_s->puissance));
+  if(data->event == 5){
+    printf("\n15 %ld %ld ", data->timestamp, data->identif_s->id);
+    for(int i = 0; i < data->id_s->size; ++i) {
+      printf("%ld ", data->id_s->tab[i]);
+    }
+  }
 }
 
 void affichageCompt(donnees_t* data) {
-	Compteur_t* compte = data->cmpt_s;
-	size_t THvalide = compte->nbrTH - compte->manifErrTH - compte->valInvTH;
-	size_t TAvalide = compte->nbrTA - compte->manifErrTA - compte->valInvTA;
-	size_t Pvalide = compte->nbrPulse - compte->manifErrPulse - compte->valInvPulse;
-	float moyTA = TAvalide > 0 ? compte->sumTA/TAvalide : 0;
-	float moyTH = THvalide > 0 ? compte->sumTH/THvalide : 0;
-	float moyP = Pvalide > 0 ? compte->sumPulse/Pvalide : 0;
+  Compteur_t* compte = data->cmpt_s;
+  size_t THvalide = compte->nbrTH - compte->manifErrTH - compte->valInvTH;
+  size_t TAvalide = compte->nbrTA - compte->manifErrTA - compte->valInvTA;
+  size_t Pvalide = compte->nbrPulse - compte->manifErrPulse - compte->valInvPulse;
+  float moyTA = TAvalide > 0 ? compte->sumTA/TAvalide : 0;
+  float moyTH = THvalide > 0 ? compte->sumTH/THvalide : 0;
+  float moyP = Pvalide > 0 ? compte->sumPulse/Pvalide : 0;
 
-	printf("\n21 %.1f %.1f %.0f",moyTH, moyTA, moyP);
-	printf("\n22 %ld %ld %ld", compte->valInvTH, compte->valInvTA, compte->valInvPulse);
-	printf("\n23 %ld %ld %ld", compte->manifErrTH/3, compte->manifErrTA/3, compte->manifErrPulse/3);
+  printf("\n21 %.1f %.1f %.0f",moyTH, moyTA, moyP);
+  printf("\n22 %ld %ld %ld", compte->valInvTH, compte->valInvTA, compte->valInvPulse);
+  printf("\n23 %ld %ld %ld", compte->manifErrTH/3, compte->manifErrTA/3, compte->manifErrPulse/3);
 }
 
